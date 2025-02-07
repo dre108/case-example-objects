2756cac64f7dee7c8bc76b3c6ff1717edef4b445
Dre Kinsey <deondre.kinsey@gmail.com>
2025-02-07 10:15:38 -0600

    Code added to update info and validate info functions which changes the way the date is displayed when entered

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function validateInput(input) {
     alert("Please fill in this field.");
     input.focus();
     return false;
-  }
+  };
   return true;
 }
 
@@ -59,11 +59,28 @@ function loadMovieFromLocalStorage() {
 
 // Unified Update Function
 function infoUpdate(input, property, textElement, displayText) {
+  const selectedDate = input.valueAsDate
+  console.log("🚀 ~ infoUpdate ~ selectedDate:", selectedDate)
+
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+const dateString = new Intl.DateTimeFormat('en-US', options).format(selectedDate);
+console.log("🚀 ~ infoUpdate ~ dateString:", dateString)
+
   if (validateInput(input)) {
-    Movie[property] = input.value.trim(); // Update the movie object
+    if (property === "releaseDate") {
+      Movie[property] = dateString; // Update the movie object
+      textElement.textContent = `${displayText} ${Movie[property]}`; // Update the DOM
+      saveMovieToLocalStorage(); // Save the updated object to local storage
+      input.value = ""; // Clear the input field // Outputs date in the user's locale format
+    }else {
+          Movie[property] = input.value.trim(); // Update the movie object
     textElement.textContent = `${displayText} ${Movie[property]}`; // Update the DOM
     saveMovieToLocalStorage(); // Save the updated object to local storage
     input.value = ""; // Clear the input field
+    }
+
+
+
   }
 }
 
